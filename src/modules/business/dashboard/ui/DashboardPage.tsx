e4352921beac7dacfa4d3384bbe3641a98cfb9b9
Sergey Avtsynov <55112113+AvtsynovS@users.noreply.github.com>
2026-04-18 15:18:10 +0300

    add dashboard router (#3)

---
+++ src/modules/business/dashboard/ui/DashboardPage.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+type DashboardPageProps = {};
+
+const StyledWrapper = styled.div`
+  display: flex;
+`;
+
+export const DashboardPage = ({}: DashboardPageProps) => {
+  return <StyledWrapper>DashboardPage</StyledWrapper>;
+};
